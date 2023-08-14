c02552111f7b2c3928dece593047bf261c634198
Kevin Wilson <kevindanielwilson@outlook.com>
2023-08-14 03:04:53 -0500

    Add pikmin code

--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,16 @@ struct Player {
     sprite: Base<Sprite2D>
 }
 
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct RedPikmin {
+    speed: f64,
+    angular_speed: f64,
+
+    #[base]
+    sprite: Base<Sprite2D>
+}
+
 
 #[godot_api]
 impl Sprite2DVirtual for Player {
@@ -41,5 +51,26 @@ impl Sprite2DVirtual for Player {
         self.sprite.translate(velocity * delta as f32);
         // The 'rotate' method requires a f32,
         // therefore we convert 'self.angular_speed * delta' which is a f64 to a f32
+    }
+}
+
+#[godot_api]
+impl Sprite2DVirtual for RedPikmin {
+    fn init(sprite: Base<Sprite2D>) -> Self {
+        godot_print!("Hello, pikmin!!!"); // Prints to the Godot console
+
+        Self {
+            speed: 100.0,
+            angular_speed: std::f64::consts::PI,
+            sprite
+        }
+    }
+
+    fn physics_process(&mut self, delta: f64) {
+        self.sprite.rotate((self.angular_speed * delta) as f32);
+
+        let rotation = self.sprite.get_rotation();
+        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
+        self.sprite.translate(velocity * delta as f32);
     }
 }
